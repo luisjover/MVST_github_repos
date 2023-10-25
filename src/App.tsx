158170f1e69e59339d264e0e82f30dd18cd4a504
Luis Jover <luisjoverdev@gmail.com>
2023-10-25 20:13:39 +0200

    feat(graphql): first tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+const { VITE_GITHUB_TOKEN: token } = import.meta.env;
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+
+    fetch("https://api.github.com/graphql", {
+      method: "POST",
+      headers: {
+        Authorization: `bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        query: `
+        query {
+          viewer {
+            name
+            avatarUrl
+            bio
+            followers {
+              totalCount
+            }
+            following {
+              totalCount
+            }
+            repositories(last: 20) {
+              nodes {
+                name
+                createdAt
+                updatedAt
+                description
+                isPrivate
+                languages(last: 4) {
+                  nodes {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+        `
+      })
+
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div>App</div>
+
   )
 }
 
